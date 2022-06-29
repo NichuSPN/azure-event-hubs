6330c92f7de6ae7c09024188900658cdb39a1cae
NishaanthSP <nishaanth.sp@swymcorp.com>
2022-06-29 10:46:01 +0530

    Remove Creds

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  eventHubNamespace: "Endpoint=sb://eventhub-test-nishaanth.servicebus.windows.net/;SharedAccessKeyName=ShareManageAccess-Test;SharedAccessKey=J33VBBxH/rdHBKrPkH93bTARmeNSv/oGwvReS/GTHkI=;EntityPath=nishaanth-eventhub-1",
-  eventHub: "nishaanth-eventhub-1" 
+  eventHubNamespace: <Connection String-Primary Key>,
+  eventHub: <Event Hub Name> 
 }
